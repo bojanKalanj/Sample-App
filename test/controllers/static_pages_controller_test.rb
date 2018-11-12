dfe5062189cd2a776bdde047d48a668e3157c5f4
bojanKalanj <bojan.kalanj@gmail.com>
2018-11-12 16:23:18 +0100

    Adds full title method to application helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @base_title = "Ruby on Rails Tutorial SampleApp"
+    @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
